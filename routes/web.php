7795ad4d3c86c0c0d33f54e879d18689420c053d
sucimariaa <suci.mariaa8898@gmail.com>
2024-07-08 17:43:33 +0700

    Mnambahkan dli line 2&3, 13-15

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Anggota2Controller;
+use App\Http\Controllers\AnggotaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
@@ -8,6 +10,16 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+Route::get('/anggota', [AnggotaController::class, 'index'])->name('anggota');
+Route::get('/tambah-anggota', [AnggotaController::class, 'create'])->name('anggota.create');
+Route::post('/tambah-anggota', [AnggotaController::class, 'store'])->name('anggota.store');
+//Route::resource('anggota', AnggotaController::class);
+
+
 Route::get('/users', function () {
     return view('/components/users-page', ['title' => 'Data Pengguna']);
 })->name('users');
+
+ //route::get('/anggota', [AnggotaController::class, 'index']);
+
+ //Route::resource('/anggota', Anggota2Controller::class);
