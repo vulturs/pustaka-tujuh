b1cd3427170de327ef88f913ab306b33312f0e5b
sucimariaa <suci.mariaa8898@gmail.com>
2024-07-17 10:22:07 +0700

    penambahan edit

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Clases;
+use App\Models\Kelas;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Laravel\Prompts\Key;
 
 class KelasController extends Controller
 {
@@ -14,16 +15,10 @@ class KelasController extends Controller
     public function index()
     {
        
-        $kelas = Clases::first();
-
-        if(request('search')){
-            $kelas->where('kelas','like','%'.request('search').'%');
-        }
-
         return view('components.kelas.kelas-page', [
             'title' => "Data Kelas",
             // 'anggota' => $anggota
-            'kelas' => $kelas->paginate(5)
+            'kelas' => Kelas::filter()->orderBy('kelas_id')->paginate(5)
         ]);
     }
 
@@ -48,7 +43,7 @@ class KelasController extends Controller
 
         $validated['excerpt'] = Str::limit($request->body,200);
 
-        Clases::create($validated);
+        Kelas::create($validated);
         return redirect()->route('kelas')->with('success', 'Data kelas berhasil ditambahkan');
     }
 
@@ -63,17 +58,39 @@ class KelasController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit($id)
     {
-        //
+        $kelas = Kelas::find($id);
+
+        // if (is_null($anggota)) {
+        //     return redirect()->route('anggota')->with('error', 'Anggota tidak ditemukan.');
+        // }
+
+        return view('components.kelas.edit-kelas-page', [
+            'title' => "Edit Data Kelas",
+            'kelas' => $kelas,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $kelas = Kelas::find($id);
+
+        if (is_null($kelas)) {
+            return redirect()->route('kelas')->with('error', 'Kelas tidak ditemukan.');
+        }
+
+        $validateData = $request->validate([
+            'kelas' => 'required|string|max:100',
+        ]);
+
+        // $anggota->update($validateData);
+        Kelas::where('kelas_id', $kelas->kelas_id)->update($validateData);
+
+        return redirect()->route('kelas')->with('success', 'Anggota berhasil diperbarui.');
     }
 
     /**
@@ -81,7 +98,7 @@ class KelasController extends Controller
      */
     public function destroy($kelas_id)
     {
-        $kelas = Clases::find($kelas_id);
+        $kelas = Kelas::find($kelas_id);
 
         if (is_null($kelas)) {
             return redirect()->back()->with('error', 'Kelas tidak ditemukan.');
